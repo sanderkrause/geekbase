6a0cac2d50cf26ab583a20d83295b1e066e66c1f
Sander Krause <sanderkrause@gmail.com>
2020-07-11 05:10:16 +0200

    Cleaned up base template. Modified icons in menu. Added dashboard controller and template.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
+     * This is intended as the home page for unauthenticated and anonymous users, including links to the login page
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * This page is the landing page for authenticated users, containing user-sensitive information
+     * @Route("/dashboard", name="dashboard")
+     * @return Response
+     */
+    public function dashboard(): Response
+    {
+        return $this->render('home/dashboard.html.twig');
     }
 }
