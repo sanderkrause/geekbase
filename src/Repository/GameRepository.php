b17224156f32dd43de1ce434a556a6c532727b40
Sander Krause <sanderkrause@gmail.com>
2020-07-13 00:25:13 +0200

    Added form for adding new games. Added page to render form. Added some fancy stats to game overview page.

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Game;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +20,61 @@ class GameRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Game::class);
+    }
+
+    public function countUserGames(User $user): int
+    {
+        $query = $this->createQueryBuilder('g')
+            ->select('count(g)')
+            ->where('g.user = :user')
+            ->setParameter(':user', $user->getId())
+            ->getQuery();
+
+        try {
+            $gameCount = (int)$query->getSingleScalarResult();
+        } catch (NoResultException $e) {
+            $gameCount = 0;
+        } catch (NonUniqueResultException $e) {
+            $gameCount = 0;
+        }
+        return $gameCount;
+    }
+
+    public function countUniqueGenres(User $user): int
+    {
+        $query = $this->createQueryBuilder('g')
+            ->select('count(distinct genres)')
+            ->andWhere('g.user = :user')
+            ->setParameter(':user', $user->getId())
+            ->leftJoin('g.genre', 'genres')
+            ->getQuery();
+
+        try {
+            $genres = (int)$query->getSingleScalarResult();
+        } catch (NoResultException $e) {
+            $genres = 0;
+        } catch (NonUniqueResultException $e) {
+            $genres = 0;
+        }
+        return $genres;
+    }
+
+    public function countSpecialEditions(User $user)
+    {
+        $query = $this->createQueryBuilder('g')
+            ->select('count(distinct g.publisher)')
+            ->andWhere('g.user = :user')
+            ->setParameter(':user', $user->getId())
+            ->getQuery();
+
+        try {
+            $publishers = (int)$query->getSingleScalarResult();
+        } catch (NoResultException $e) {
+            $publishers = 0;
+        } catch (NonUniqueResultException $e) {
+            $publishers = 0;
+        }
+        return $publishers;
     }
 
     // /**
