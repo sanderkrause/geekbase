d3104390c8ae686340acd4240576d2981a186e81
Sander Krause <sanderkrause@gmail.com>
2020-08-15 17:00:32 +0200

    Removed unused code in GameFormType that was made redundant by using a security voter instead.

--- src/Form/GameFormType.php
+++ src/Form/GameFormType.php
@@ -13,20 +13,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Core\Security;
 
 class GameFormType extends AbstractType
 {
-    /**
-     * @var Security
-     */
-    private $security;
-
-    public function __construct(Security $security)
-    {
-        $this->security = $security;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
