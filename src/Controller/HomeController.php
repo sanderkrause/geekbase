c4a6de98d7a85d86e7e20c9d50ae856750a3bf7e
Sander Krause <sanderkrause@gmail.com>
2020-08-31 00:49:53 +0200

    Added dummy text to landing page. Added basic statistics to landing page. Fixed minor navigation issue.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,15 @@
 
 namespace App\Controller;
 
+use App\Repository\BoardGameRepository;
+use App\Repository\BookRepository;
+use App\Repository\GameRepository;
+use App\Repository\GenreRepository;
+use App\Repository\MangaRepository;
+use App\Repository\MovieRepository;
+use App\Repository\PublisherRepository;
+use App\Repository\SerieRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +20,40 @@ class HomeController extends AbstractController
     /**
      * This is intended as the home page for unauthenticated and anonymous users, including links to the login page
      * @Route("/", name="home")
+     *
+     * @param GameRepository $gameRepository
+     * @param UserRepository $userRepository
+     * @param MovieRepository $movieRepository
+     * @param SerieRepository $serieRepository
+     * @param BookRepository $bookRepository
+     * @param BoardGameRepository $boardGameRepository
+     * @param MangaRepository $mangaRepository
+     * @param GenreRepository $genreRepository
+     * @param PublisherRepository $publisherRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(GameRepository $gameRepository, UserRepository $userRepository, MovieRepository $movieRepository, SerieRepository $serieRepository, BookRepository $bookRepository, BoardGameRepository $boardGameRepository, MangaRepository $mangaRepository, GenreRepository $genreRepository, PublisherRepository $publisherRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $totalGames = $gameRepository->count([]);
+        $totalUsers = $userRepository->count([]);
+        $totalMovies = $movieRepository->count([]);
+        $totalSeries = $serieRepository->count([]);
+        $totalBooks = $bookRepository->count([]);
+        $totalMangas = $mangaRepository->count([]);
+        $totalBoardGames = $boardGameRepository->count([]);
+        $totalGenres = $genreRepository->count([]);
+        $totalPublishers = $publisherRepository->count([]);
+        return $this->render('home/index.html.twig', [
+            'totalGames' => $totalGames,
+            'totalUsers' => $totalUsers,
+            'totalMovies' => $totalMovies,
+            'totalSeries' => $totalSeries,
+            'totalBooks' => $totalBooks,
+            'totalMangas' => $totalMangas,
+            'totalBoardGames' => $totalBoardGames,
+            'totalGenres' => $totalGenres,
+            'totalPublishers' => $totalPublishers,
+        ]);
     }
 
     /**
